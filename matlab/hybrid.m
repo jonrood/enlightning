6e2ad16a0c5eb7ae5cca281a34e8b8ca35501af7
Jon Rood <jonrood@server.fake>
2014-01-17 14:17:47 -0800

    Adding license info to matlab files.

--- matlab/hybrid.m
+++ matlab/hybrid.m
@@ -1,3 +1,22 @@
+% ===========================================================================
+% Copyright (C) 2012 Jon Rood.
+%
+% This file is part of Enlightning source code.
+% 
+% Enlightning source code is free software; you can redistribute it
+% and/or modify it under the terms of the GNU General Public License as
+% published by the Free Software Foundation; either version 3 of the License,
+% or (at your option) any later version.
+% 
+% Enlightning source code is distributed in the hope that it will be
+% useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with Enlightning; if not, see <http://www.gnu.org/licenses/>.
+% ===========================================================================
+
 function hybrid(timeSteps,X,Y,srcPosX,srcPosY,...
     srcAmp,srcAlpha,maxFreq,ppw,cfl,bc,rc)
 
